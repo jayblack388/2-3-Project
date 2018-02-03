84bbc6d819b2dc293f5a4d0221a79a51dced0b6d
zrcarter2017 <zcarter@allegiancy.us>
2018-02-03 11:22:58 -0500

    routes configured

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -8,6 +9,18 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.get("/index.html", function(req, res) {
+	res.sendFile(path.join(__dirname, "html/index.html"));
+})
+
+app.get("/reserve.html", function(req, res) {
+	res.sendFile(path.join(__dirname, "html/reserve.html"));
+})
+
+app.get("/tables.html", function(req, res) {
+	res.sendFile(path.join(__dirname, "html/tables.html"));
+})
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
